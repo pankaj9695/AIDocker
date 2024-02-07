46d798c77bf23a630a6fe27394462c2cf66b7886
sps03 <shantanusonwane20@cse.iiitp.ac.in>
2024-02-07 19:31:44 +0530

    meta image and description added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,54 @@
+// import Header from "@/components/Header";
+// import Hero from "@/components/Hero";
+// import Last from "@/components/Last";
+// import Problem from "@/components/Problem";
+// // import StyledComponent from "@/components/StyledComponent";
+// import Cursor from "@/components/Cursor";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <Cursor />
+//       <Header />
+//       <Hero />
+//       <Problem />
+//       <Last />
+//     </main>
+//   );
+// }
+
+import Head from 'next/head';
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Last from "@/components/Last";
 import Problem from "@/components/Problem";
-// import StyledComponent from "@/components/StyledComponent";
 import Cursor from "@/components/Cursor";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>AI Docker</title>
+        <meta name="description" content="From data-driven enthusiasts to seasoned developers, AiDocker is
+                the go-to destination for creating, training, and deploying AI
+                models with ease and efficiency.
+              </motion.p>" />
+        <meta property="og:image" content="aidocker.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:description" content="From data-driven enthusiasts to seasoned developers, AiDocker is
+                the go-to destination for creating, training, and deploying AI
+                models with ease and efficiency.
+              </motion.p>" />
+        <meta property="og:title" content="AI Docker" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="aidocker.jpg" />
+        <meta name="twitter:description" content="From data-driven enthusiasts to seasoned developers, AiDocker is
+                the go-to destination for creating, training, and deploying AI
+                models with ease and efficiency." />
+        <meta name="twitter:title" content="AI Docker" />
+      </Head>
       <Cursor />
       <Header />
       <Hero />
